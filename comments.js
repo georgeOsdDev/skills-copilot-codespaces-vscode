68c26cafe2b2dabf27f99899b2828907a1fad77a
Takeharu.Oshida <georgeosddev@gmail.com>
2023-09-26 05:03:22 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// create web server with express
+const express = require('express');
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
+app.get('/comments', (req, res) => {
+    res.send('Comments');
+}
+
